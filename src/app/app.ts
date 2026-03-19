0beebc3abd726602ac3fec2059c1e0a5cbbfa1a7
Gustavo-RDias <gustavoribeirodiasj8@gmail.com>
2026-03-19 12:04:09 -0300

    prmeiro estado da pagina certificado gerados finalizado

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,11 +4,12 @@ import { PrimaryButton } from "./_components/primary-button/primary-button";
 import { SecondaryButton } from './_components/secondary-button/secondary-button';
 import { ItemCertificado } from './_components/item-certificado/item-certificado';
 import { BaseUi } from "./_components/base-ui/base-ui";
+import { Certificados } from './pages/certificados/certificados';
 
 
 @Component({
   selector: 'app-root',
-  imports: [Navebar, PrimaryButton, SecondaryButton, ItemCertificado, BaseUi],
+  imports: [Navebar, PrimaryButton, SecondaryButton, ItemCertificado, BaseUi, Certificados],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/pages/certificados/certificados.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SecondaryButton } from "../../_components/secondary-button/secondary-button";
+
+@Component({
+  selector: 'app-certificados',
+  imports: [SecondaryButton],
+  templateUrl: './certificados.html',
+  styleUrl: './certificados.scss',
+})
+export class Certificados {
+
+}
