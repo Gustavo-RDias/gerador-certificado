60c71e2ceb01175d0bee77ddc1ec3ac9c1c78c02
Gustavo-RDias <gustavoribeirodiasj8@gmail.com>
2026-03-20 12:01:55 -0300

    Desenvolvimento de formulário que captura informações do usuário

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,11 +5,12 @@ import { SecondaryButton } from './_components/secondary-button/secondary-button
 import { ItemCertificado } from './_components/item-certificado/item-certificado';
 import { BaseUi } from "./_components/base-ui/base-ui";
 import { Certificados } from './pages/certificados/certificados';
+import { CertificadoForm } from "./pages/certificado-form/certificado-form";
 
 
 @Component({
   selector: 'app-root',
-  imports: [Navebar, PrimaryButton, SecondaryButton, ItemCertificado, BaseUi, Certificados],
+  imports: [Navebar, PrimaryButton, SecondaryButton, ItemCertificado, BaseUi, Certificados, CertificadoForm],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
