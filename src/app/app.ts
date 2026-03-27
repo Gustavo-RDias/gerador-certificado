63eb586c51e06662b218d32f7a2a4466d9e9643b
Gustavo-RDias <gustavoribeirodiasj8@gmail.com>
2026-03-27 12:40:19 -0300

    feat: adiciona página de certificado

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,11 +6,12 @@ import { ItemCertificado } from './_components/item-certificado/item-certificado
 import { BaseUi } from "./_components/base-ui/base-ui";
 import { Certificados } from './pages/certificados/certificados';
 import { CertificadoForm } from "./pages/certificado-form/certificado-form";
+import { Certificado } from "./pages/certificado/certificado";
 
 
 @Component({
   selector: 'app-root',
-  imports: [Navebar, PrimaryButton, SecondaryButton, ItemCertificado, BaseUi, Certificados, CertificadoForm],
+  imports: [Navebar, PrimaryButton, SecondaryButton, ItemCertificado, BaseUi, Certificados, CertificadoForm, Certificado],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
